623b5adceef820e4ea91988cd076cf8c526da745
Matheus Pescador <matheus.pescador2@gmail.com>
2022-12-14 19:37:46 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -8,6 +8,8 @@ public abstract class ChessPiece extends Piece {
 
     private Color color;
 
+    private int moveCount;
+
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
@@ -17,12 +19,26 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+
+    }
+
+
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
     protected boolean isThereOpponentPiece(Position position) {
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
 }
